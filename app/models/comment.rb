e648b796632cdfead62a0d5966bf3f7ff14a53c7
Brandon Block <branblock@gmail.com>
2015-12-12 17:19:15 -0800

    voting for comments added

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,8 @@ class Comment < ActiveRecord::Base
   belongs_to :post
   belongs_to :user
 
+  acts_as_votable
+
   validates :body, length: { minimum: 5 }, presence: true
 
   default_scope { order('updated_at ASC') }
