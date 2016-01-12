db5ebdd7f08f4e17782d8dbfca8fccb52591a300
Brandon Block <branblock@gmail.com>
2016-01-12 11:56:09 -0800

    minor styling fixes

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -2,7 +2,7 @@ CKEDITOR.editorConfig = function( config ) {
     // Define changes to default configuration here. For example:
     // config.language = 'fr';
     config.uiColor = '#ffffff';
-    config.font_names = 'Open Sans;' + 'Arial;' + 'Courier New;' + 'Times New Roman;' + 'Tahoma;' + 'Verdana';
+    config.font_names = 'Helvetica;' + 'Arial;' + 'Courier New;' + 'Times New Roman;' + 'Tahoma;' + 'Verdana';
 
     config.toolbar_Post = [
       { name: 'top', items : [ 'Source','DocProps','Preview','Maximize','PasteFromWord','-','Undo','Redo','Bold','Italic','Underline','Strike','Subscript','Superscript','-','RemoveFormat','NumberedList','BulletedList','-','Outdent','Indent','-','Blockquote','-','JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock','-','Link','Unlink','Table','SpecialChar','PageBreak' ] },
